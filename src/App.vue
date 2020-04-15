5f53a3c23e2f91cda6cfa9739f914c96be2f5f59
purple <purple29b@gmail.com>
2020-04-15 17:28:24 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="app">
+      <TodoHeader></TodoHeader>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+      <TodoFooter></TodoFooter>
+
+    </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader';
+import TodoFooter from './components/TodoFooter';
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
+
+export default {
+  components:{
+    'TodoHeader' : TodoHeader,
+    'TodoFooter' : TodoFooter,
+    'TodoList' : TodoList,
+    'TodoInput' : TodoInput
+  }
+    
+}
+</script>
+
+<style scoped>
+    body{
+        text-align: center;
+        background-color: #f6f6f8;
+    }
+    input{
+        border-style:groove;
+        width: 200px;
+    }
+    button{
+        border-style: groove;
+    }
+    .shadow{
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+    }
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        this.clearInput();
+      }else{
+          return false;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
